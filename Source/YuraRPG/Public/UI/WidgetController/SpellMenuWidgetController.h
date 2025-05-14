7f312336b94f54e7e6f027761035ff9f2e37ff8c
Enhoney <1904456753@qq.com>
2025-05-14 23:08:17 +0800

    Spell Menu

---
+++ Source/YuraRPG/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Muci Yun.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/YuraWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YURARPG_API USpellMenuWidgetController : public UYuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
